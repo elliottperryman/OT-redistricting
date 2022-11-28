17127a7c0f68f915f1de120b0b1b4aef07a69f13
elliott <elliott.perryman@hey.com>
2022-11-28 18:19:56 +0100

    fig?

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ block, tract, county, state = load_pop_df(10)
 df = discrete(tract)
 K = 3
 centers = np.array([rand_guess(state) for i in range(K)]) 
-C = np.array([df['geometry'].to_crs(3857).distance(p) for p in centers])
+C = np.array([df['geometry'].distance(p) for p in centers]).T
+C /= np.max(C)
 
 N = len(df)
 a, b = np.ones(N)/N, np.ones(K)/K
 
-res = sinkhorn(C, 1e-3, a, b, 10)
+res = sinkhorn(C, 1e-3, a, b, 100)
 print(res)
