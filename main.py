5ba75dca65eaf31c8ccadd9d11a0bbb9611c2b7d
elliott <elliott.perryman@hey.com>
2022-11-29 16:27:47 +0100

    working on discrete case

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-import numpy as np
 
-from algs import load_pop_df, discrete, rand_guess, sinkhorn
-
-block, tract, county, state = load_pop_df(10)
-
-df = discrete(tract)
-K = 3
-centers = np.array([rand_guess(state) for i in range(K)]) 
-C = np.array([df['geometry'].distance(p) for p in centers]).T
-C /= np.max(C)
-
-N = len(df)
-a, b = np.ones(N)/N, np.ones(K)/K
-
-res = sinkhorn(C, 1e-3, a, b, 100)
-print(res)
