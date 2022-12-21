45d1e7ce35ffb148bc06fa26b4a84555a31c998d
bdb <bdbonnaires@gmail.com>
2022-12-22 00:08:10 +0100

    added helper function

--- download.py
+++ download.py
@@ -9,7 +9,7 @@ import tempfile
 # nice progress bars
 from tqdm import tqdm
 
-def download_state_number(state_number):
+def download_shapefile_bynumber(state_number):
     """
     Download US states shapefiles data from 
     https://www2.census.gov/geo/tiger/TIGER2020/TABBlOCK20/
@@ -95,3 +95,13 @@ def download_state_number(state_number):
         ## UNZIPPING THE FILE TO DATA FOLDER
         with zipfile.ZipFile(zipfile_path, "r") as file:
             file.extract(member=fileName, path=datadir)
+
+def download_all_shapefiles():
+    """
+    Helper function to download all states shapefiles.
+    """
+
+    states_dict= { "ALABAMA": 1, "ALASKA": 2, "ARIZONA": 4, "ARKANSAS": 5, "CALIFORNIA": 6, "COLORADO": 8, "CONNECTICUT": 9, "DELAWARE": 10, "DISTRICT_OF_COLUMBIA": 11, "FLORIDA": 12, "GEORGIA": 13, "HAWAII": 15, "IDAHO": 16, "ILLINOIS": 17, "INDIANA": 18, "IOWA": 19, "KANSAS": 20, "KENTUCKY": 21, "LOUISIANA": 22, "MAINE": 23, "MARYLAND": 24, "MASSACHUSETTS": 25, "MICHIGAN": 26, "MINNESOTA": 27, "MISSISSIPPI": 28, "MISSOURI": 29, "MONTANA": 30, "NEBRASKA": 31, "NEVADA": 32, "NEW_HAMPSHIRE": 33, "NEW_JERSEY": 34, "NEW_MEXICO": 35, "NEW_YORK": 36, "NORTH_CAROLINA": 37, "NORTH_DAKOTA": 38, "OHIO": 39, "OKLAHOMA": 40, "OREGON": 41, "PENNSYLVANIA": 42, "RHODE_ISLAND": 44, "SOUTH_CAROLINA": 45, "SOUTH_DAKOTA": 46, "TENNESSEE": 47, "TEXAS": 48, "UTAH": 49, "VERMONT": 50, "VIRGINIA": 51, "WASHINGTON": 53, "WEST_VIRGINIA": 54, "WISCONSIN": 55, "WYOMING": 56, "PUERTO_RICO": 72}
+    for state in states_dict:
+        print(f"Downloading {state}...")
+        download_shapefile_bynumber(states_dict[state])
