19415d8466fe33824b9234c2fbd1a6fefe223bad
bdb <bdbonnaires@gmail.com>
2022-12-22 21:51:54 +0100

    fixed typo

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 # Creates and sources the virtual environment then install the required packages
 # If the directory already exists, just sources the venv
-if [ -d ./Sem]; then 
+if [ -d ./Sem ]; then 
 	source ./Sem/bin/activate;
 else
 	python -m venv Sem
