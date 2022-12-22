e86eee266b9c01a5f0a6ac01893ab6c25640dfff
bdb <bdbonnaires@gmail.com>
2022-12-22 10:30:55 +0100

    added exceptions for error handling

--- download.py
+++ download.py
@@ -9,6 +9,10 @@ import tempfile
 # nice progress bars
 from tqdm import tqdm
 
+class ShapeFileAlreadyDownloadedError(Exception):
+    def __init__(self):
+        super().__init__("State Shapefile already downloaded.")
+
 def download_shapefile_bynumber(state_number):
     """
     Download US states shapefiles data from 
@@ -77,8 +81,7 @@ def download_shapefile_bynumber(state_number):
     if os.path.isdir(datadir):
         # tests if the file is already there
         if os.path.isfile(f"{datadir}/{fileName}"):
-            print("State already downloaded.")
-            return
+            raise ShapeFileAlreadyDownloadedError()
     else:
             os.mkdir(datadir)
 
@@ -109,4 +112,8 @@ def download_all_shapefiles():
     states_dict= { "ALABAMA": 1, "ALASKA": 2, "ARIZONA": 4, "ARKANSAS": 5, "CALIFORNIA": 6, "COLORADO": 8, "CONNECTICUT": 9, "DELAWARE": 10, "DISTRICT_OF_COLUMBIA": 11, "FLORIDA": 12, "GEORGIA": 13, "HAWAII": 15, "IDAHO": 16, "ILLINOIS": 17, "INDIANA": 18, "IOWA": 19, "KANSAS": 20, "KENTUCKY": 21, "LOUISIANA": 22, "MAINE": 23, "MARYLAND": 24, "MASSACHUSETTS": 25, "MICHIGAN": 26, "MINNESOTA": 27, "MISSISSIPPI": 28, "MISSOURI": 29, "MONTANA": 30, "NEBRASKA": 31, "NEVADA": 32, "NEW_HAMPSHIRE": 33, "NEW_JERSEY": 34, "NEW_MEXICO": 35, "NEW_YORK": 36, "NORTH_CAROLINA": 37, "NORTH_DAKOTA": 38, "OHIO": 39, "OKLAHOMA": 40, "OREGON": 41, "PENNSYLVANIA": 42, "RHODE_ISLAND": 44, "SOUTH_CAROLINA": 45, "SOUTH_DAKOTA": 46, "TENNESSEE": 47, "TEXAS": 48, "UTAH": 49, "VERMONT": 50, "VIRGINIA": 51, "WASHINGTON": 53, "WEST_VIRGINIA": 54, "WISCONSIN": 55, "WYOMING": 56, "PUERTO_RICO": 72}
     for state in states_dict:
         print(f"Downloading {state}...")
-        download_shapefile_bynumber(states_dict[state])
+        try:
+            download_shapefile_bynumber(states_dict[state])
+        except ShapeFileAlreadyDownloadedError as e:
+            print(str(e))
+            continue
