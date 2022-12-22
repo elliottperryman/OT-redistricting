5146f205a2aa3e2ee5654f010927394a52d4222b
bdb <bdbonnaires@gmail.com>
2022-12-22 19:26:31 +0100

    changed logic

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
 # Creates and sources the virtual environment then install the required packages
+# If the directory already exists, just sources the venv
 if [ -d ./Sem]; then 
 	source ./Sem/bin/activate;
 else
 	python -m venv Sem
 	source ./Sem/bin/activate;
+	python -m pip -r requirements.txt
 fi
 
-python -m pip -r requirements.txt
