2ffb2c7fa5619db4a7e54dbc17a2352fcc325d20
bdb <bdbonnaires@gmail.com>
2022-12-26 09:06:16 +0100

    removed semicolons

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 # Creates and sources the virtual environment then install the required packages
 # If the directory already exists, just sources the venv
 if [ -d ./Sem ]; then 
-	source ./Sem/bin/activate;
+	source ./Sem/bin/activate
 else
 	python -m venv Sem
-	source ./Sem/bin/activate;
+	source ./Sem/bin/activate
 	python -m pip -r requirements.txt
 fi
 
