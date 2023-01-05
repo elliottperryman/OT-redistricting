ecb0f828418a4d6d306f93a0d02180b5cce22475
elliott <elliott.perryman@hey.com>
2023-01-05 12:47:37 +0100

    put level in state

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from State import State
 state = State(1) # use alabama because I have it downloaded
 from lloyd_alg import lloyd_alg
-districts = lloyd_alg(state, 'tract', 10)
+districts = lloyd_alg(state, 10)
