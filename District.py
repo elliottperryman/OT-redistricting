a9f0bc54c43625471f18a519044f6495af5eb58c
elliott <elliott.perryman@hey.com>
2023-01-05 11:33:18 +0100

    add some extra functionality for districts

--- District.py
+++ District.py
@@ -1,17 +1,23 @@
 from State import State
 import matplotlib.pyplot as plt
+import numpy as np
 
 class District():
-    def __init__(self, state: State, res, df, cost_val, centers):
+    def __init__(self, state: State, res, df, centers):
         self.state = state
         self.res = res
         self.df = df
-        self.cost_val = cost_val
         self.centers = centers
 
-    def plot(self):
+    def score(self):
+        return np.sum(np.array([self.df.distance(p) for p in self.centers]).T * self.res)
+    
+    def plot(self, plot=True):
+        self.df = self.df.dissolve(by='district', aggfunc='sum')
         self.df.plot(column=self.df.index.values, categorical=True, legend=True)
         plt.ylabel('Longitude')
         plt.xlabel('Latitude')
         plt.title(self.state.pretty_name)
         plt.scatter([_.x for _ in self.centers], [_.y for _ in self.centers])
+        if plot:
+            plt.show()
