9ffdc8819946ea6a60759b7fc5270b83b71605ca
elliott <elliott.perryman@hey.com>
2023-01-05 11:33:39 +0100

    add a sampler that chooses points proportional to population

--- misc.py
+++ misc.py
@@ -5,6 +5,7 @@ Algorithms for solving redistricting by optimal transport
 ## Standard imports
 import numpy as np
 import shapely
+from geopandas import GeoSeries
 
 def rand_guess(state):
     """
@@ -14,3 +15,10 @@ def rand_guess(state):
     LB, UB = np.array([minx, miny]), np.array([maxx, maxy])
     p = shapely.geometry.Point(*(np.random.random()*(UB-LB)+LB))
     return p
+
+def sample_rand(df, K):
+    centroids = df.to_crs(crs=3857).centroid.to_crs(4269).values
+    chosen = np.random.choice(a=len(df), size=K, replace=False, p=df['pop'].values)
+    samples =  GeoSeries(centroids[chosen])
+    samples.set_crs(4269)
+    return samples
