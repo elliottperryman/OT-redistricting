781c9a63a9df265fafc2e96441fe651573ac8216
elliott <elliott.perryman@hey.com>
2023-01-05 17:50:02 +0100

    some changes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from os import system
+system('cd data && bash get.sh')
 from State import State
-state = State(1) # use alabama because I have it downloaded
+state = State(23) # use alabama because I have it downloaded
 from lloyd_alg import lloyd_alg
 districts = lloyd_alg(state, 10)
