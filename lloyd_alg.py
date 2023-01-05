95ea41cd8a4016d6c322f74e9195924e876bc631
elliott <elliott.perryman@hey.com>
2023-01-05 11:34:20 +0100

    can supply iteration count

--- lloyd_alg.py
+++ lloyd_alg.py
@@ -1,9 +1,9 @@
 from State import State
 from discrete_alg import solve_discrete
 
-def lloyd_alg(state: State, lvl='tract'):
+def lloyd_alg(state: State, lvl='tract', iterations=10):
     districts = [solve_discrete(state, lvl)]
-    for i in range(4):
+    for i in range(iterations-1):
         try:
             districts.append(solve_discrete(state, lvl, districts[-1].centers))
         except RuntimeError as err:
