684568b11e0008f9e01cbb89be39cec63ab2e227
elliott <elliott.perryman@hey.com>
2023-01-11 12:01:47 +0100

    don't need numba, and come comments

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from State import State
-state = State(23) # use alabama because I have it downloaded
-from lloyd_alg import lloyd_alg
-districts = lloyd_alg(state, 10)
+state = State(23) # use maine because it has only 2 districts
+from discrete_alg import solve_discrete
+district = solve_discrete(state)
+
+# from lloyd_alg import lloyd_alg
+# districts = lloyd_alg(state, 10)
